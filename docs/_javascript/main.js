106f613a18e237bb4138e046dc08bb54c31d8593
Jack Holloway <jack@mrprimate.co.uk>
2022-01-27 21:47:18 +0000

    Correct tile click javascript

--- docs/_javascript/main.js
+++ docs/_javascript/main.js
@@ -1,7 +1,6 @@
 let tiles = document.getElementsByClassName("tile is-child notification");
 for (let tile of tiles) {
   tile.addEventListener("click", function () {
-    console.warn(tile);
-    window.location.href = "./status.html";
+    window.location.href = `#${tile.getAttribute("bookcode")}`;
   });
 }
